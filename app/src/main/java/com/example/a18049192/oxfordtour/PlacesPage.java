db4bffbeefbd9575682e7735dfb6834aba89b4ea
Pete <peterspanner@hotmail.com>
2019-11-20 12:24:24 +0000

    Button functionality added but commented out until pages are merged. Images for locations added.

--- app/src/main/java/com/example/a18049192/oxfordtour/PlacesPage.java
+++ app/src/main/java/com/example/a18049192/oxfordtour/PlacesPage.java
@@ -1,7 +1,10 @@
 package com.example.a18049192.oxfordtour;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PlacesPage extends AppCompatActivity {
 
@@ -9,6 +12,35 @@ public class PlacesPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_places_page);
-        //test commit 
+
+        /*Button ashmuseum = findViewById(R.id.AshMusButton);
+        ashmuseum.setOnClickListener(new View.OnClickListener(){
+            //loads the page for the Ashmolean Museum from the first button on the page.
+            @Override
+            public void onClick(View v) {
+                Intent ashpage = new Intent(this, AshmoleanMuseum.class);
+                startActivity(ashpage);
+            }
+        });
+
+        Button christbutton = findViewById(R.id.ChristChurch);
+        christbutton.setOnClickListener(new View.OnClickListener(){
+            //loads the page for the Ashmolean Museum from the first button on the page.
+            @Override
+            public void onClick(View v) {
+                Intent ph2page = new Intent(this, ChristChurch.class);
+                startActivity(ph2page);
+            }
+        });
+
+        Button magdalenbutton = findViewById(R.id.MagdalenCollege);
+        magdalenbutton.setOnClickListener(new View.OnClickListener(){
+            //loads the page for the Ashmolean Museum from the first button on the page.
+            @Override
+            public void onClick(View v) {
+                Intent ph3page = new Intent(this, MagdalenCollege.class);
+                startActivity(ph3page);
+            }
+        });*/
     }
 }
