cbb6ce6bad571b9eaf8c13d86e42295f08ff4495
Pete <peterspanner@hotmail.com>
2020-04-05 20:59:17 +0100

    Sprint 7

--- app/src/main/java/com/example/a18049192/oxfordtour/ForumPage.java
+++ app/src/main/java/com/example/a18049192/oxfordtour/ForumPage.java
@@ -10,17 +10,47 @@ import android.widget.TextView;
 public class ForumPage extends AppCompatActivity {
 
     TextView faq;
+    TextView general;
+    TextView fplaces;
+    TextView frestaurants;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forum_page);
         faq = findViewById(R.id.FAQText);
+        general = findViewById(R.id.ForumGeneral);
+        fplaces = findViewById(R.id.ForumPlaces);
+        frestaurants = findViewById(R.id.ForumRestaurants);
 
         faq.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), FAQPage.class);
+                startActivity(intent);
+            }
+        });
+
+        general.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Forum_General.class);
+                startActivity(intent);
+            }
+        });
+
+        fplaces.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Forum_Places.class);
+                startActivity(intent);
+            }
+        });
+
+        frestaurants.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Forum_Restaurants.class);
                 startActivity(intent);
             }
         });
